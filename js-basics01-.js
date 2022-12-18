656b0dbd2ceaa492ac49e5027989feb8005e4338
Mahalakshmi84 <120233445+Mahalakshmi84@users.noreply.github.com>
2022-12-17 21:09:19 -0500

    js basics

---
+++ js-basics01-.js
@@ -0,0 +1,54 @@
+var name = 'Adam' //string
+name = 'Eva'
+
+var age = 10 //number
+
+var email = 'shn@test.com'
+var address = '123 Main St, San Jose CA 95132'
+var a = 10
+var sum = 10+20 // 30
+var x = a+10 // '20'
+
+var foo = 'hello' + 'world' // 'hello world'
+
+var isHuman = true //boolean
+isHuman = false
+
+var isVaccinated = false
+
+var ageOfPerson = 'eve@test.com'
+
+var x1 = 1
+var y1 = 2
+
+
+var z1 = x1 + y1 //3
+
+var z2 = z1-x1 // 2
+
+console.log('Hello World')
+
+console.log(x)
+
+console.log(10+40)
+
+console.log('a' + 45)
+
+var xx = 100
+var yy = 200
+xx = xx/2 //50
+console.log(xx/10);
+
+console.log( xx == 100)
+
+var ss = 10
+
+if(ss > 10) {
+    console.log('Equals')
+ } else {
+    console.log('Not Equals')
+ }
+
+ for(var i = 0; i<5; i++) {
+    console.log('Equals')
+ }
